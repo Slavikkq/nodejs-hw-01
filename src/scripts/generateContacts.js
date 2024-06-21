3ca7641a546ac66f69a23bfa0f96af8e58de625c
Slavikkq <yaroslavmanuljak@gmail.com>
2024-06-21 13:59:37 +0300

    node hw 01

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,5 +1,23 @@
+import fs from 'fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
 
-const generateContacts = async (number) => {};
+const generateContacts = async (number) => {
+  try {
+    const dataContacts = await fs.readFile(PATH_DB, 'utf-8');
+    const contacts = JSON.parse(dataContacts);
 
-generateContacts(5);
+    for (let i = 0; i < number; i++) {
+      contacts.push(createFakeContact());
+    }
+
+    const newContacts = JSON.stringify(contacts, null, 2);
+    await fs.writeFile(PATH_DB, newContacts, 'utf-8');
+
+    console.log(`Successfully added ${number} contacts.`);
+  } catch (error) {
+    console.log('Error:', error);
+  }
+};
+
+await generateContacts(5);
--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,5 +1,19 @@
+import fs from 'fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
 
-export const removeLastContact = async () => {};
+export const removeLastContact = async () => {
+  try {
+    const dataContacts = await fs.readFile(PATH_DB, 'utf-8');
+    const contacts = JSON.parse(dataContacts);
+    const randomContacts = contacts.filter(() => Math.random() > 0.5);
+    await fs.writeFile(
+      PATH_DB,
+      JSON.stringify(randomContacts, null, 2),
+      'utf-8',
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-removeLastContact();
+await removeLastContact();
